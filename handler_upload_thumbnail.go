3dd003e6697f2d14d56cfb70aeffb59fc7d43f12
Sean Huebl <sean.huebl@gmail.com>
2024-12-31 06:03:46 -0700

    added ability to upload thumbnail image. working on encoding the whole image data into a base64 string that we can store in sqlite3

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,45 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to parse file", err)
+		return
+	}
+	defer file.Close()
+	mediaType := header.Header.Get("Content-type")
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to read media data", err)
+		return
+	}
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to locate video", err)
+		return
+	}
+	thumbnail := thumbnail{
+		data:      data,
+		mediaType: mediaType,
+	}
+	videoThumbnails[videoID] = thumbnail
+
+	thumbnailURL := fmt.Sprintf("localhost:%v/api/thumbnails/%v", cfg.port, videoID)
+	metaData.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(metaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to update metadata", err)
+		return
+	}
+	imgDataString := base64.StdEncoding.EncodeToString(thumbnail.data)
+	dataURL := fmt.Sprintf("data:%v;base64,%v", thumbnail.mediaType, imgDataString)
+
+	respondWithJSON(w, http.StatusOK, metaData)
 }
