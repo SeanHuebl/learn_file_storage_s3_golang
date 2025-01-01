e938a070de6385a94751629854073655d9fb75b5
Sean Huebl <sean.huebl@gmail.com>
2025-01-01 06:00:22 -0700

    working on upload video handler. added AWS SDK to project for future use as file server

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,65 @@
 package main
 
 import (
+	"fmt"
+	"mime"
 	"net/http"
+
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	const maxUploadLimit = 1 << 30
+
+	r.Body = http.MaxBytesReader(w, r.Body, maxUploadLimit)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error finding metadata", err)
+		return
+	}
+	if metaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "access denied", err)
+		return
+	}
+
+	fmt.Println("uploading footage for video", videoID, "by user", userID)
+	r.ParseMultipartForm(maxUploadLimit)
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to form file", err)
+		return
+	}
+	defer file.Close()
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to parse media type", err)
+		return
+	}
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "file must be of type .mp4", err)
+		return
+	}
+
+	// save the upload to temp file on disk
+}
